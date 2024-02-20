5ede46d96913a720e2b6b82eabdc8a3b0fa266c4
Tharin Sandul <124861676+sandul2023@users.noreply.github.com>
2024-02-20 19:49:37 +0530

    assets added

--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -1,6 +1,12 @@
-import React from "react";
+import React,{ useState } from "react";
 
 const Navbar = () => {
+    const [nav, setNav] = useState(false);
+    const [logo, setLogo] = useState(false)
+    const handleNav = () => {
+        setNav(!nav);
+        setLogo(!logo)
+    };
     return (
         <></>
     )
