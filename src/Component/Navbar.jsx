feac75ff5be887cc654a13bb86725e449391017a
Tharin Sandul <124861676+sandul2023@users.noreply.github.com>
2024-02-22 04:29:16 +0530

    navbar update

--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -1,4 +1,8 @@
 import React,{ useState } from "react";
+import {BsPerson} from "react-icons/bs";
+import {BiSearch} from "react-icons/bi";
+import {HiOutlineMenuAlt4} from "react-icons/hi";
+import {AiOutlineClose} from "react-icons/ai";
 
 const Navbar = () => {
     const [nav, setNav] = useState(false);
@@ -19,8 +23,29 @@ const Navbar = () => {
                 <li>View</li>
                 <li>Book</li>
             </ul>
+            <div className='hidden md:flex'>
+                <BiSearch className='' size={20}/>
+                <BsPerson size={20}/>
+            </div>
+
+            /*blabla*/
+            <div onClick={handleNav} className='md:hidden z-10'>
+                {nav ? <AiOutlineClose className='text-black' size={20}/> : <HiOutlineMenuAlt4 size={20}/>}
+            </div>
+
+            /*Menu Dropdown*/
+            <div onClick={handleNav} className={nav ? 'absolute text-black left-0 top-0 w-full bg-gray-100/90 px-4 py-7 flex flex-col' : 'absolute left-[-100%]'}>
+                <ul>
+                    <h1>BEACHES.</h1>
+                    <li className='border-b'>Home</li>
+                    <li className='border-b'>Destinations</li>
+                    <li className='border-b'>Travel</li>
+                    <li className='border-b'>View</li>
+                    <li className='border-b'>Book</li>
+                </ul>
+            </div>
         </div>
-    )
+    );
 }
 
 export default Navbar;
