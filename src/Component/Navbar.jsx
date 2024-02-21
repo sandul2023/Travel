879c841bdc17e5b6df3e4b7938efe76109147109
Tharin Sandul <124861676+sandul2023@users.noreply.github.com>
2024-02-22 04:23:16 +0530

    navbar funcitons

--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -8,7 +8,18 @@ const Navbar = () => {
         setLogo(!logo)
     };
     return (
-        <></>
+        <div className='flex w-full justify-between items-center h-20 px-4 absolute z-10 text-white'>
+            <div>
+                <h1 onClick={handleNav} className={logo ? 'hidden' : 'block'}>BEACHES.</h1>
+            </div>
+            <ul className='hidden md:flex'>
+                <li>Home</li>
+                <li>Destinations</li>
+                <li>Travel</li>
+                <li>View</li>
+                <li>Book</li>
+            </ul>
+        </div>
     )
 }
 
