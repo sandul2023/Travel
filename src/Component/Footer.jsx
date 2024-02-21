a824f6038134db562fa339c54f4e832c6a12e7d9
Tharin Sandul <124861676+sandul2023@users.noreply.github.com>
2024-02-22 04:58:06 +0530

    footer section updated

--- src/Component/Footer.jsx
+++ src/Component/Footer.jsx
@@ -5,13 +5,31 @@ const Footer = () => {
     return (
         <div className='w-full bg-gray-100 py-16'>
             <div className='max-w-[1240px] mx-auto flex flex-col px-4'>
-                <h1>BEACHES.</h1>
-                <div className='flex justify-between w-full sm:max-w-[280px] my-4'>
-                    <FaFacebook className='icon'/>
-                    <FaTwitter className='icon'/>
-                    <FaYoutube className='icon'/>
-                    <FaPinterest className='icon'/>
-                    <FaInstagram className='icon'/>
+                <div className='sm:flex text-center justify-between items-center'>
+                    <h1>BEACHES.</h1>
+                    <div className='flex justify-between w-full sm:max-w-[280px] my-4'>
+                        <FaFacebook className='icon'/>
+                        <FaTwitter className='icon'/>
+                        <FaYoutube className='icon'/>
+                        <FaPinterest className='icon'/>
+                        <FaInstagram className='icon'/>
+                    </div>
+                </div>
+                <div className='flex justify-between'>
+                    <ul className='lg:flex'>
+                        <li>About</li>
+                        <li>Partnerships</li>
+                        <li>Careers</li>
+                        <li>Newsroom</li>
+                        <li>Advertising</li>
+                    </ul>
+                    <ul className='lg:flex'>
+                        <li>About</li>
+                        <li>Partnerships</li>
+                        <li>Careers</li>
+                        <li>Newsroom</li>
+                        <li>Advertising</li>
+                    </ul>
                 </div>
             </div>
         </div>
