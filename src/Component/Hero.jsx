cd45ae4defc94f6f7cfd4bc64fb0ddf05057b7eb
Tharin Sandul <124861676+sandul2023@users.noreply.github.com>
2024-02-23 04:56:13 +0530

    Updated

--- src/Component/Hero.jsx
+++ src/Component/Hero.jsx
@@ -18,7 +18,7 @@ const Hero = () => {
             <div className='absolute w-full h-full top-0 left-0 bg-gray-900/30'></div>
             <div className='absolute top-0 w-full h-full flex flex-col justify-center text-center text-white p-4'>
                 <h1>First Class Travel</h1>
-                <h2 className='py-4'>Top 1% Locations Worldwide</h2>
+                <h2 className='py-4'>Best Locations In Sri Lanka</h2>
                 <form
                     className='flex justify-between items-center max-w-[700px] mx-auto w-full border p-1
           rounded-md text-black bg-gray-100/90'
