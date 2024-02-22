33574e4be029ffe73f71980624c0735102543fe0
Tharin Sandul <124861676+sandul2023@users.noreply.github.com>
2024-02-23 04:42:21 +0530

    UI Updated

--- src/Component/Hero.jsx
+++ src/Component/Hero.jsx
@@ -1,15 +1,19 @@
 import React from "react";
 import {AiOutlineSearch} from "react-icons/ai";
-import beachVid from '../assets/beachVid.mp4'
+import Train from '../assets/Train.mp4'
+import Sigiriya from '../assets/Sigiriya.mp4'
 const Hero = () => {
     return(
         <div className='w-full h-screen relative'>
             <video
                 className='w-full h-full object-cover'
-                src={beachVid}
+                src={Train}
+                onDurationChange={Train}
+                onDurationChangeCapture={Sigiriya}
                 autoPlay
                 loop
                 muted
+                du
             />
             <div className='absolute w-full h-full top-0 left-0 bg-gray-900/30'></div>
             <div className='absolute top-0 w-full h-full flex flex-col justify-center text-center text-white p-4'>
