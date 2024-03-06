fc8c0f636eceb2d2dcfddfdad8fcad0eb9e5e6f6
Tharin Sandul <124861676+sandul2023@users.noreply.github.com>
2024-03-06 17:14:24 +0530

    Content Update

--- src/Component/Search.jsx
+++ src/Component/Search.jsx
@@ -8,17 +8,17 @@ const Search = () => {
         <div className='max-w-[1240px] mx-auto grid lg:grid-cols-3 gap-4 px-4 py-16'>
             <div className='lg:col-span-2 flex flex-col justify-evenly'>
                 <div>
-                    <h2>LUXURY INCLUDED VACATIONS FOR TWO PEOPLE</h2>
+                    <h2>BEST LUXURY ROUND TOUR FOR TWO PEOPLE</h2>
                     <p className='py-4'>
-                        Come experience the very pinnacle of luxury Caribbean all-inclusive
-                        vacations for couples at BEACHES Resorts. Our luxury beach resorts,
+                        Come experience the best tour of luxury Vehicle all-inclusive
+                        vacations for couples at BEACHES Resorts and Villas. Our luxury beach resorts and villas,
                         set along the most gorgeous tropical settings and exquisite beaches
-                        in Saint Lucia, Jamaica, Antigua, The Bahamas, Grenada, Barbados and
-                        Curaçao, feature unlimited gourmet dining, unique bars serving
+                        in Hikkaduwa, Galle, Unawatuna, Mirissa, Weligama and
+                        Arugambay feature unlimited gourmet dining, unique bars serving
                         premium liquors and wines, and every land and water sport, including
                         complimentary green fees at our golf resorts and certified scuba
                         diving at most resorts. If you are planning a wedding, BEACHES is
-                        the leader in Caribbean destination weddings and honeymoon packages.
+                        the leader in Sri Lankan destination weddings and honeymoon packages.
                     </p>
                 </div>
                 <div className='grid sm:grid-cols-2 gap-8 py-4'>
