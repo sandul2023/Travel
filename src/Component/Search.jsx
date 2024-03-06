58f34dcdaed274d61015b868b1127e42ffc5c6c5
Tharin Sandul <124861676+sandul2023@users.noreply.github.com>
2024-03-06 16:49:43 +0530

    Search Updated

--- src/Component/Search.jsx
+++ src/Component/Search.jsx
@@ -1,5 +1,7 @@
 import React from "react";
-import {RiCustomerService2Full, RiCustomerService2Fill} from "react-icons/ri";
+import {RiCustomerService2Fill} from "react-icons/ri";
+import {MdOutlineTravelExplore} from "react-icons/md";
+
 
 const Search = () => {
     return(
@@ -24,8 +26,40 @@ const Search = () => {
                         <button>
                             <RiCustomerService2Fill size={50}/>
                         </button>
+                        <div>
+                            <h3 className='py-2'>LEADING SERVICE</h3>
+                            <p className='py-1'>ALL-INCLUSIVE COMPANY FOR 20 YEARS IN-A-ROW</p>
+                        </div>
+                    </div>
+                    <div className='flex flex-col lg:flex-row items-center text-center'>
+                        <button>
+                            <MdOutlineTravelExplore size={50}/>
+                        </button>
+                        <div>
+                            <h3 className='py-2'>LEADING SERVICE</h3>
+                            <p className='py-1'>ALL-INCLUSIVE COMPANY FOR 20 YEARS IN-A-ROW</p>
+                        </div>
                     </div>
                 </div>
+            </div>
+
+            <div>
+                <div className='border text-center'>
+                    <p className='pt-2'>GET AN ADDITIONAL 10% OFF</p>
+                    <p className='py-4'>12 HOURS LEFT</p>
+                    <p className='bg-gray-800 text-gray-200 py-2'>BOOK NOW AND SAVE</p>
+                </div>
+                <form className='w-full'>
+                    <div className='flex flex-col my-2'>
+                        <label>Destination</label>
+                        <select className='border rounded-md p-2'>
+                            <option>Grande Antigua</option>
+                            <option>Key West</option>
+                            <option>Maldives</option>
+                            <option>Cozumel</option>
+                        </select>
+                    </div>
+                </form>
             </div>
 
         </div>
